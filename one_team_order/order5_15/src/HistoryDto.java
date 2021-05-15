225d8711c99d427f1a128eca403888fd5d5ff340
zoo1046 <83991671+zoo1046@users.noreply.github.com>
2021-05-16 02:05:11 +0900

    Add files via upload

---
+++ one_team_order/order5_15/src/HistoryDto.java
@@ -0,0 +1,53 @@
+public class HistoryDto {
+	int hno;
+	String hid;
+	String hmenu;
+	String hsize;
+	String hoption;
+	String hprice;
+	String hdate;
+	
+	public int getHno() {
+		return hno;
+	}
+	public void setHno(int hno) {
+		this.hno = hno;
+	}
+	public String getHid() {
+		return hid;
+	}
+	public void setHid(String hid) {
+		this.hid = hid;
+	}
+	public String getHmenu() {
+		return hmenu;
+	}
+	public void setHmenu(String hmenu) {
+		this.hmenu = hmenu;
+	}
+	public String getHsize() {
+		return hsize;
+	}
+	public void setHsize(String hsize) {
+		this.hsize = hsize;
+	}
+	public String getHoption() {
+		return hoption;
+	}
+	public void setHoption(String hoption) {
+		this.hoption = hoption;
+	}
+	public String getHprice() {
+		return hprice;
+	}
+	public void setHprice(String hprice) {
+		this.hprice = hprice;
+	}
+	public String getHdate() {
+		return hdate;
+	}
+	public void setHdate(String hdate) {
+		this.hdate = hdate;
+	}
+	
+}
